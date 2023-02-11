eeb29c5efeb486768230850f1526b75dbcd5bf60
Farzad <fsunavala@microsoft.com>
2023-02-11 11:52:55 -0600

    search bar home

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,13 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import Header from "@/components/Header";
+import { useRouter } from "next/router";
+import SearchPage from "./search";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function HomePage() {
   return (
     <>
       <Head>
@@ -19,7 +22,35 @@ export default function Home() {
       </Head>
       <Header />
       <main>
-        <h1>this is athena</h1>
+        <div className="h-32 bg-black flex items-center flex-col">
+          <div className="text-white text-xl text-center">
+            {" "}
+            <span className="text-3xl font-bold">welcome</span>
+            <br />
+            to athena
+          </div>
+          <div className="text-white flex mt-4">
+            <p>powered by OpenAI</p>
+            <Image
+              alt="azs icon"
+              src="/azs.png"
+              style={{ objectFit: "contain" }}
+              width={50}
+              height={30}
+            />
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          {" "}
+          <div className="flex items-center justify-center rounded-lg -mt-4 p-3 bg-white shadow-md w-3/5">
+            <MagnifyingGlassIcon width={20} className="text-gray-500" />
+            <input
+              type="text"
+              className="bg-[#FFFFFFCC] focus:outline-0 w-full px-1"
+              placeholder={"Type a search query"}
+            />
+          </div>
+        </div>
       </main>
     </>
   );
---
+++ pages/search.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+type Props = {}
+
+export default function SearchPage({}: Props) {
+  return (
+    <>
+    
+    </>
+
+  )
+}
