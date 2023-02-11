47f434e700495e67c1d0dd69a968601410deddc2
Farzad <fsunavala@microsoft.com>
2023-02-11 10:10:45 -0600

    header

---
+++ components/Header.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import React from "react";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <header className="flex justify-between items-center px-10 py-6 shadow-lg">
+      {/* Logo */}
+      <div className="flex gap-1">
+        <h1 className="flex-none uppercase font-bold text-2xl">Athena</h1>
+        <div className="uppercase text-sm text-start">beta</div>
+      </div>
+
+      <nav className="flex-1 text-lg">
+        <ul className="flex justify-center gap-10">
+          <li className="transition duration-300 ease-out hover:ease-in hover:text-blue-600">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="transition duration-300 ease-out hover:ease-in hover:text-blue-600">
+            <Link href="/search">Search</Link>
+          </li>
+          <li className="transition duration-300 ease-out hover:ease-in hover:text-blue-600">
+            <Link href="/tips">Tips</Link>
+          </li>
+        </ul>
+      </nav>
+      {/* Login or AVATAR */}
+      {/* <UserCircleIcon width={40} /> */}
+      <button className="rounded-3xl bg-blue-600 px-4 py-2 text-white w-36 transition duration-300 ease-out hover:ease-in hover:bg-blue-400">
+        Sign In
+      </button>
+    </header>
+  );
+};
+
+export default Header;
